c3c46a595d20de719be45773ee866487ee88fe9a
David Keeter <davidpkeeter@gmail.com>
2023-04-20 18:13:23 -0500

    updated invokeUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://50us0454u1.execute-api.us-east-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          ' https://54xy1gb5h3.execute-api.us-east-2.amazonaws.com/Prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
